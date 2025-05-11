3c4513f39e4c99540aa456da9122cde3398c0e86
Georgi Fudulov <fudulov.georgi@gmail.com>
2025-05-11 19:55:59 +0300

    Made the Header responsive

--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -1,25 +1,55 @@
-import '../styles/Header.css'
+import { useState } from "react";
+import "../styles/Header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClose = () => setIsOpen(false);
+
   return (
-    <header className="wrapper header">
-      <h1 className='logo'><a href='#hero'>Georgi Fudulov</a></h1>
-      <nav className='wrapper'>
-        <ul className="wrapper list" role='list'>
-          <li>
-            <a href="#about">About</a>
-          </li>
-          <li>
-            <a href="#what-i-do">What I Do</a>
-          </li>
-          <li>
-            <a href="#projects">Projects</a>
-          </li>
-          <li>
-            <a href="#contacts">Contacts</a>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header className="wrapper header">
+        <h1 className="logo">
+          <a href="#hero" onClick={handleClose}>
+            Georgi Fudulov
+          </a>
+        </h1>
+
+        <button
+          className="menu-toggle"
+          onClick={() => setIsOpen((prev) => !prev)}
+          aria-label="Toggle menu"
+        >
+          <FontAwesomeIcon icon={isOpen ? faXmark : faBars} />
+        </button>
+
+        <nav className={`wrapper nav ${isOpen ? "open" : ""}`}>
+          <ul className="wrapper list" role="list">
+            <li>
+              <a href="#about" onClick={handleClose}>
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#what-i-do" onClick={handleClose}>
+                What I Do
+              </a>
+            </li>
+            <li>
+              <a href="#projects" onClick={handleClose}>
+                Projects
+              </a>
+            </li>
+            <li>
+              <a href="#contacts" onClick={handleClose}>
+                Contacts
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      {isOpen && <div className="backdrop" onClick={handleClose}></div>}
+    </>
   );
 }
